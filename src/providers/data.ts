d6b5b3352219fc324b9b87a4ff01e4c1b4cecc46
Dzaky Fadli <zaaaafl654@gmail.com>
2026-02-24 10:52:49 +0700

    implement data provider

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,34 @@
-import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
-});
+import { MOCK_SUBJECTS } from "@/constants/mock-data";
+import {
+  BaseRecord,
+  DataProvider,
+  GetListParams,
+  GetListResponse,
+} from "@refinedev/core";
+
+export const dataProvider: DataProvider = {
+  getList: async <TData extends BaseRecord = BaseRecord>({
+    resource,
+  }: GetListParams): Promise<GetListResponse<TData>> => {
+    if (resource !== "subjects") return { data: [] as TData[], total: 0 };
+
+    return {
+      data: MOCK_SUBJECTS as unknown as TData[],
+      total: MOCK_SUBJECTS.length,
+    };
+  },
+  getOne: async () => {
+    throw new Error("This function is not present in mock");
+  },
+  create: async () => {
+    throw new Error("This function is not present in mock");
+  },
+  update: async () => {
+    throw new Error("This function is not present in mock");
+  },
+  deleteOne: async () => {
+    throw new Error("This function is not present in mock");
+  },
+
+  getApiUrl: () => "",
+};
